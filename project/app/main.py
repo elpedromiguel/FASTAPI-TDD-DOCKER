7b7f07165361040c75ce95adf5931c7cf3fd5f1f
Pedro Reyes <preyes@vidaplatform.com>
2021-05-12 15:12:10 -0400

    course

---
+++ project/app/main.py
@@ -0,0 +1,23 @@
+import os
+
+from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
+
+from app.api import ping
+
+def create_application() -> FastAPI:
+    application = FastAPI()
+    
+    register_tortoise(
+        application,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models" : ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True
+    )
+    
+    application.include_router(ping.router)
+    
+    return application
+
+app = create_application
---
+++ project/tests/conftest.py
@@ -0,0 +1,24 @@
+import os
+
+import pytest
+from starlette.testclient import TestClient
+
+from app.main import create_application  # updated
+from app.config import get_settings, Settings
+
+
+def get_settings_override():
+    return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    # set up
+    app = create_application()  # new
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:  # updated
+
+        # testing
+        yield test_client
+
+    # tear down
